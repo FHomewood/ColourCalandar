0f562a14aa2277250c62493ded04df4ce0eab448
FHomewood <FHomewood98@gmail.com>
2020-08-25 19:50:06 +0100

    screen now auto scales to the primary monitor

--- Scheduler/Scheduler/MainWindow.xaml.cs
+++ Scheduler/Scheduler/MainWindow.xaml.cs
@@ -20,24 +20,36 @@ namespace Scheduler
 {
     public partial class MainWindow : Window
     {
-        public const int WS_EX_TRANSPARENT = 0x0000020; public const int GWL_EXSTYLE = (-20);
+        // Constants used for the window style
+        public const int WS_EX_TRANSPARENT = 0x0000020;
+        public const int GWL_EXSTYLE = (-20);
 
+        // Import necessary DLLs for the click-through semi-transparent window
         [DllImport("user32.dll")]
         public static extern int GetWindowLong(IntPtr hwnd, int index);
 
         [DllImport("user32.dll")]
         public static extern int SetWindowLong(IntPtr hwnd, int index, int newStyle);
 
-
+        //Initialize timer
         DispatcherTimer dispatcherTimer = new DispatcherTimer();
+
+        //Colour variables
         double hue = 0;
         int transition = 1;
 
         public MainWindow()
         {
             InitializeComponent();
+            this.Width = System.Windows.SystemParameters.PrimaryScreenWidth;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight;
+
+
+            //Set time interval
             dispatcherTimer.Interval = new TimeSpan(10 * 1000 * 10);
+            //Add the tick event
             dispatcherTimer.Tick += dispatcherTimer_Tick;
+            //Begin the timer
             dispatcherTimer.Start();
         }
 
@@ -57,29 +69,43 @@ namespace Scheduler
 
         void dispatcherTimer_Tick(object sender, EventArgs e)
         {
+
             int mSecondsInTransition = 100;
             int framesInTransition = 50;
             double hueChange = 1f * Math.PI / 3f;
 
+            //If we're mid-transition between scenes
             if (transition > 0)
             {
                 transition--;
+                //set the timer according to the specified framerate
                 dispatcherTimer.Interval = new TimeSpan(10 * 1000 * mSecondsInTransition / framesInTransition);
+
+                //Change the hue
                 hue += hueChange / (double)framesInTransition;
+                //apply the new colour according to the hue formulae
                 this.Background = new SolidColorBrush(Color.FromArgb(
                     24,
                     (byte)(128 + 127 * Math.Cos(hue)),
                     (byte)(128 + 127 * Math.Cos(hue + 2 * Math.PI / 3)),
                     (byte)(128 + 127 * Math.Cos(hue + 4 * Math.PI / 3))
                 ));
+
+                //if this is the end of the transition we set the next check to the next next minute (to the dot)
                 if (transition == 0)
                     dispatcherTimer.Interval = new TimeSpan(10 * 1000 * (1000 - DateTime.Now.Millisecond) + 10 * 1000 * 1000 * (59-DateTime.Now.Second));
             }
+            //If we're not mid-transition between scenes
+            //Check if we're due to transition
             else if (DateTime.Now.Minute % 10 != 0)
+                //not due? check again in 1 minute
                 dispatcherTimer.Interval = new TimeSpan(10 * 1000 * 1000 * 60);
+            //If we're due a transition
             else
             {
+                //set the total number of transition frames
                 transition = framesInTransition;
+                //reset quickly so the transition can be initiated
                 dispatcherTimer.Interval = new TimeSpan(1);
             }
         }
