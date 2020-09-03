d75693687d70f4a147d6437d90aa461be7b1df42
Frankie Homewood <fhomewood98@gmail.com>
2020-09-03 10:44:26 +0100

    Displaying the csv pulled plan to the plannerwindow now, formatting adjustments still necessary.

--- Scheduler/Scheduler/PlannerWindow.xaml.cs
+++ Scheduler/Scheduler/PlannerWindow.xaml.cs
@@ -29,10 +29,11 @@ namespace Scheduler
             _timer.Tick += tick;
             this.parent = parent;
 
-            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight / 2;
-            this.Width  = System.Windows.SystemParameters.PrimaryScreenWidth  / 2;
-            grid.Height = this.Height;
-            grid.Width = this.Width;
+            this.Height = System.Windows.SystemParameters.PrimaryScreenHeight*0.75;
+            this.Width  = System.Windows.SystemParameters.PrimaryScreenWidth*0.75;
+            grid.Height = this.Height - 4 * parent.cm;
+            grid.Width = this.Width-7*parent.cm;
+            grid.Margin = new Thickness(6 * parent.cm, 3 *parent.cm, 1 * parent.cm, 0);
 
 
 
@@ -45,15 +46,63 @@ namespace Scheduler
 
         private void DrawSchedule()
         {
+            double minutesinaday = 60 * 24;
+
             for (int i = 0; i < parent.csv.Count; i++)
             {
-                for (int j = 0; j < parent.csv[i].Task.Length; j++)
+                for (int j = -1; j < parent.csv[i].Task.Length; j++)
                 {
+                    double rectmargins = 10;
                     Rectangle rect = new Rectangle();
-                    rect.Height = 10;
-                    rect.Width = 10;
+                    Label lbl = new Label();
                     rect.Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                    rect.Height = grid.Height / 7 - 2 * rectmargins;
+
+
+                    double taskoffset;
+                    double taskwidth;
+                    if (j == -1)
+                    {
+                        taskwidth = (parent.csv[i].Time.First().Hour - 6) * 60 + (parent.csv[i].Time.First().Minute);
+                        taskwidth *= grid.Width / minutesinaday;
+                        taskoffset = 0;
+                        try { lbl.Content = parent.csv[i - 1].Task.Last(); }
+                        catch { lbl.Content = ""; }
+                    }
+                    else if (parent.csv[i].Time[j] == parent.csv[i].Time.Last())
+                    {
+                        taskwidth = (30 - parent.csv[i].Time[j].Hour) * 60 + (60 - parent.csv[i].Time[j].Minute);
+                        taskwidth *= grid.Width / minutesinaday;
+                        lbl.Content = parent.csv[i].Task[j];
+                        taskoffset = (parent.csv[i].Time[j].Hour * 60
+                                      - 360
+                                      + parent.csv[i].Time[j].Minute  )
+                            * grid.Width / minutesinaday;
+                    }
+                    else
+                    {
+                        taskwidth = (parent.csv[i].Time[j + 1].Hour - parent.csv[i].Time[j].Hour) * 60
+                                    + (parent.csv[i].Time[j + 1].Minute - parent.csv[i].Time[j].Minute);
+                        taskwidth *= grid.Width / minutesinaday;
+                        lbl.Content = parent.csv[i].Task[j];
+                        taskoffset = (parent.csv[i].Time[j].Hour * 60
+                                      - 360
+                                      + parent.csv[i].Time[j].Minute  )
+                            * grid.Width / minutesinaday;
+                    }
+
+                    rect.Margin = new Thickness(taskoffset, rectmargins + (rect.Height + 2 * rectmargins - 2) * i,0,0);
+                    lbl.Margin = rect.Margin;
+                    rect.Width = taskwidth - 2;
+                    rect.VerticalAlignment = VerticalAlignment.Top;
+                    rect.HorizontalAlignment = HorizontalAlignment.Left;
+                    TransformGroup labelTransform = new TransformGroup();
+                    labelTransform.Children.Add(new RotateTransform(-90));
+                    labelTransform.Children.Add(new TranslateTransform(0, -2 * rectmargins));
+                    lbl.RenderTransform = labelTransform;
+                    
                     grid.Children.Add(rect);
+                    grid.Children.Add(lbl);
                 }
             }
         }
@@ -61,9 +110,9 @@ namespace Scheduler
         public void Transition()
         {
             showing = !showing;
-
+            if (this.Visibility == Visibility.Hidden) this.Visibility = Visibility.Visible;
             begin = DateTime.Now;
-            end = begin.AddSeconds(1);
+            end = begin.AddSeconds(0.75);
             _timer.Start();
         }
 
@@ -72,20 +121,23 @@ namespace Scheduler
         private void ChangeHeight()
         {
             double transitionPercentage = (begin - DateTime.Now).TotalSeconds / (begin - end).TotalSeconds;
-            double hFactor;
             double showingloc = System.Windows.SystemParameters.PrimaryScreenHeight / 2 - this.Height / 2;
             double hiddenloc = -this.Height;
-            hFactor = (1 - Math.Pow(Math.Cos(Math.PI * transitionPercentage / 2), 2));
 
             if (transitionPercentage < 1)
             {
+                double hFactor = (1 - Math.Pow(Math.Cos(Math.PI * transitionPercentage / 2), 2));
                 if (showing) this.Top = hiddenloc + hFactor * (showingloc - hiddenloc);
                 else this.Top = showingloc + hFactor * (hiddenloc - showingloc);
             }
             else
             {
                 if (showing) this.Top = showingloc;
-                else this.Top = hiddenloc;
+                else
+                {
+                    this.Top = hiddenloc;
+                    this.Visibility = Visibility.Hidden;
+                }
             }
         }
 
